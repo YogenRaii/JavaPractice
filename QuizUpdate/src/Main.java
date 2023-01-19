77947cc3fa4d3175631e9dae449f6870636d0910
Yogen Rai <yogen.rai.992@gmail.com>
2023-01-18 21:21:07 -0600

    Refactor class to make it more readable

--- QuizUpdate/src/Main.java
+++ QuizUpdate/src/Main.java
@@ -1,14 +1,15 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-class startTest{
-    //prints with a new line
+
+class Quiz {
+    
     void print(String message){
         System.out.println(message);
     }
 
 
-    public void testMethod(){
+    public void play(){
 
         Questions q1= new Questions("\nWhat is a correct syntax to output \"Hello World\" in Java?",
                 "1: print (\"Hello World\")","2: System.out.println(\"Hello World\");",
@@ -27,50 +28,46 @@ class startTest{
                 "1: while (x > y)","2: x > y while {", "3: while x > y {","4: while x > y:");
 
 
-        //Hash map to store questions and choices:stored as a integer 1,2,3,4
-        Map<Questions,Integer> hmap=new HashMap<>();
-        hmap.put(q1,2);
-        hmap.put(q2,4);
-        hmap.put(q3,1);
-        hmap.put(q4,3);
-        hmap.put(q5,1);
+        // Hash map to store questions and choices:stored as a integer 1,2,3,4
+        Map<Questions, Integer> questionToCorrectAns = new HashMap<>();
+        questionToCorrectAns.put(q1,2);
+        questionToCorrectAns.put(q2,4);
+        questionToCorrectAns.put(q3,1);
+        questionToCorrectAns.put(q4,3);
+        questionToCorrectAns.put(q5,1);
 
-        Scanner sc= new Scanner(System.in);
-        int correctCount=0;
+        Scanner sc = new Scanner(System.in);
+        
+        int correctCount = 0;
 
         //iterate the map to print out question and choice out for all 5 questions in the map
-        for(Map.Entry<Questions,Integer> map:hmap.entrySet()){
-            print(map.getKey().getQuestion());
-            print(map.getKey().getChoice1());
-            print(map.getKey().getChoice2());
-            print(map.getKey().getChoice3());
-            print(map.getKey().getChoice4());
+        for(Map.Entry<Questions,Integer> questionAndAns: questionToCorrectAns.entrySet()){
+            print(questionAndAns.getKey().getQuestion());
+            print(questionAndAns.getKey().getChoice1());
+            print(questionAndAns.getKey().getChoice2());
+            print(questionAndAns.getKey().getChoice3());
+            print(questionAndAns.getKey().getChoice4());
             print("Select an option: ");
 
-            //get answer choice from user
-            Integer answer=sc.nextInt();
+            Integer answer = sc.nextInt();
 
             //checking user input integer answer against correct answer from hashmap
-            int correctAnswer=Integer.compare(answer, map.getValue());
-            if(correctAnswer==0){
+            if(answer == questionAndAns.getValue()){
                 print("Correct Answer");
                 correctCount++;
-
-            }
-            else{
+            } else {
                 print("Wrong Answer");
             }
 
-
-
         }
-        print("Score percentage: "+(100*correctCount)/hmap.size()+"%");
+        print("Score percentage: "+(100*correctCount)/questionToCorrectAns.size()+"%");
 
     }
 }
+
 public class Main {
     public static void main(String[] args) {
-        startTest q= new startTest ();
-        q.testMethod();
+        Quiz quiz = new Quiz();
+        quiz.play();
     }
 }
